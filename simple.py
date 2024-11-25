144590873ba290a6c003acb925f515f6d0602b34
0dpe <149327603+0dpe@users.noreply.github.com>
2024-11-25 13:11:46 -0500

    Update and rename xinput.py to simple.py

--- simple.py
+++ simple.py
@@ -5,7 +5,7 @@ SCREEN_X = 1919
 
 g = vgamepad.VX360Gamepad()
 def on_move(x, _):
-    x = max(0, min(SCREEN_X, x))
+    x = max(0, min(SCREEN_X, x)) # Clamp x between 0 to SCREEN_X
     g.right_joystick(int((x*(65535/SCREEN_X))-32768), 0) # -32768 instead of -32767.5 because at x=0, joystick should be at -32768
     g.update()
 

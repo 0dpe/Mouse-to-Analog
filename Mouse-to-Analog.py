80278506845519384326fcc8816c078c98584d68
0dpe <149327603+0dpe@users.noreply.github.com>
2024-12-01 23:02:05 -0500

    Update Mouse-to-Analog.py

--- Mouse-to-Analog.py
+++ Mouse-to-Analog.py
@@ -9,12 +9,12 @@ ctypes.windll.shcore.SetProcessDpiAwareness(2)
 
 # Screen width in pixels
 # Since indexing starts at 0, this is the usual value minus 1
-SCREEN_WIDTH = 1919
+SCREEN_WIDTH = 2559
 
 # Number of pixels mouse pointer should move to produce a maximum joystick tilt
 # Larger values = less sensitive, smaller values = more sensitive
 # Shouldn't exceed half of SCREEN_WIDTH, otherwise you won't be able to produce maximum joystick tilts
-SENSITIVITY = 200
+SENSITIVITY = 700
 
 # Mouse button to reset current mouse pointer position to zero joystick tilt
 # This will reposition the mouse pointer if any part of its range of positions that produce joystick tilts clips the edge of the screen
@@ -44,7 +44,7 @@ def on_move(x, y):
                 neutral = x - SENSITIVITY
                 joystick_tilt = 32767
         
-        gamepad.right_joystick(joystick_tilt, 0)
+        gamepad.left_joystick(joystick_tilt, 0)
         gamepad.update()
 
 def reset(x, y):
@@ -61,7 +61,7 @@ def on_click(x, y, button, pressed):
             global pause
             pause = not pause
             if pause:
-                gamepad.right_joystick(0, 0)
+                gamepad.left_joystick(0, 0)
                 gamepad.update()
             else:
                 reset(x, y)
